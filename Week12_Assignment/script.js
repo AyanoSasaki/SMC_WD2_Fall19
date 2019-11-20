1bcecef2f827967dfdcc29d0a2b852ec6b862c6d
Ayano <sasaki_ayano@student.smc.edu>
2019-11-19 18:26:25 -0800

    Week12_Assignmenr
    Added class for button and p

--- Week12_Assignment/script.js
+++ Week12_Assignment/script.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
-    $( "button" ).click(function() {
-        $( "p" ).slideToggle( "slow" );
+    $( '.b1' ).click(function() {
+        $( '.p1' ).slideToggle( "slow" );
+    });
+    $( '.b2' ).click(function() {
+        $( '.p2' ).slideToggle( "slow" );
+    });
+    $( '.b3' ).click(function() {
+        $( '.p3' ).slideToggle( "slow" );
+    });
+    $( '.b4' ).click(function() {
+        $( '.p4' ).slideToggle( "slow" );
+    });
+    $( '.b5' ).click(function() {
+        $( '.p5' ).slideToggle( "slow" );
+    });
+    $( '.b6' ).click(function() {
+        $( '.p6' ).slideToggle( "slow" );
+    });
+    $( '.b7' ).click(function() {
+        $( '.p7' ).slideToggle( "slow" );
+    });
+    $( '.b8' ).click(function() {
+        $( '.p8' ).slideToggle( "slow" );
+    });
+    $( '.b9' ).click(function() {
+        $( '.p9' ).slideToggle( "slow" );
     });
 });
